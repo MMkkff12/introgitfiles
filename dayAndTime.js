4dac046c8c63fc93c9548393b1bfe3a6a25cbf74
MMkkff12 <mark.k.farrell@gmail.com>
2020-01-30 15:50:46 -0600

    updates to date and time calculators

---
+++ dayAndTime.js
@@ -0,0 +1,16 @@
+//?This JS page will be a util for other HTML pages
+//! future use
+
+
+var start = new Date();
+console.log(start);
+
+function dAndT(){
+    var dt= new Date();
+    var today = (dt.getMonth()+1)+"-"+dt.getDate()+"-"+dt.getFullYear();
+    console.log(today);
+    var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+    console.log(time); 
+    document.getElementById("time").innerHTML = today + " " + time;
+}
+
