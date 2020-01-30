3d0489fcf4cc7ebb10a1a7a797ba60ded8cb6a40
MMkkff12 <mark.k.farrell@gmail.com>
2020-01-30 12:28:30 -0600

    created simple website with all resources

---
+++ strings.js
@@ -0,0 +1,11 @@
+function help(){
+document.getElementById("demo").innerHTML = "Thanks for Helping Me"
+alert("Thank You For Saving My Life, A Shark Was Going To Eat All of Us Alive !!!!!")
+
+}
+
+function dontHelp(){
+    document.getElementById("demo").innerHTML = "Why Won't You Help Me"
+    alert("Five People Were Eaten By A Baby Shark and Mama Shark Watched It All !!!!!")
+
+}
